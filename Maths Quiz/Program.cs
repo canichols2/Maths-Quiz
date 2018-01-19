0ec1fbc5ba7c28de28547c871eba790d5a54182f
Cody Nichols <canichols2@gmail.com>
2018-01-18 19:27:21 -0700

    changed all the code. finished assignment

--- Maths Quiz/Program.cs
+++ Maths Quiz/Program.cs
@@ -14,6 +14,7 @@ namespace Maths_Quiz
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
